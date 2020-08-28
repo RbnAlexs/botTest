5f225bc628a80050570e14a2c05727dbdde33c48
Ruben Sanchez <rubensanchezcolin@gmail.com>
2020-08-28 16:08:45 -0500

    Fix variables

--- chatbot/structjson.js
+++ chatbot/structjson.js
@@ -22,7 +22,7 @@
 
 function jsonToStructProto(json) {
   const fields = {};
-  for (const k in json) {
+  for (let k in json) {
     fields[k] = jsonValueToProto(json[k]);
   }
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const app = express ();
 const bodyParser = require('body-parser');
 
+const app = express ();
 
 
 app.use( bodyParser.json() );
